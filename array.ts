861e3367aca5e20eec6aa400b9827c38c5eca8d5
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-06-07 07:02:41 +0100

    defining array types in place

--- array.ts
+++ array.ts
@@ -10,7 +10,11 @@ const names: string[] = []; // or names: Array<string>
 const ages: number[] = []; // or ages: Array<number>
 const users: User[] = []; // array of user objs where their properties are name and isLoggedIn
 // User[][]
-const twoDArray: Array<User>[] = [[{ name: "Daniel", isLoggedIn: true }]];
+const twoDArray: Array<{
+  name: string;
+  isLoggedIn: bool;
+  isDeveloper?: bool;
+}>[] = [[{ name: "Daniel", isLoggedIn: true }]];
 
 names.push("Daniel");
 ages.push(4);
