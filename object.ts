30232133bca27628e6d9f9bd40856872a8cf5be7
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-06-07 05:04:45 +0100

    type alias

--- object.ts
+++ object.ts
@@ -1,19 +1,32 @@
-const User = {
-  name: "Daniel Olatunde",
-  email: "olatundedaniel943@gmail.com",
-  isActive: true,
+// const User = {
+//   name: "Daniel Olatunde",
+//   email: "olatundedaniel943@gmail.com",
+//   isActive: true,
+// };
+
+// const newUser = { name: "Daniel", isPaid: true, isLog: true };
+
+// function createUser(user: { name: string; isPaid: boolean }) {}
+
+// createUser(newUser);
+
+// // infer to return obj
+// function createCourse(): { name: string; isPaid: boolean } {
+//   return { name: "Typescript", isPaid: false };
+// }
+// console.log(createCourse());
+
+//-----------------------------------------------
+// type alias
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
 };
 
-const newUser = { name: "Daniel", isPaid: true, isLog: true };
-
-function createUser(user: { name: string; isPaid: boolean }) {}
-
-createUser(newUser);
-
-// infer to return obj
-function createCourse(): { name: string; isPaid: boolean } {
-  return { name: "Typescript", isPaid: false };
+// take obj of type of User and return obj of type of User as well
+function createUser(user: User): User {
+  return { name: "", email: "", isActive: false };
 }
-console.log(createCourse());
-
+createUser({ name: "", email: "", isActive: false });
 export {};
