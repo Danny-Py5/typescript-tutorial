4a102df49f462d83d44d93231c89179dd4a6c81a
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-06-07 21:47:59 +0100

    Ts Class

--- tsfile_p/dist/index.js
+++ tsfile_p/dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("Hello world");
+class User {
+    constructor(name, email, id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.increaseUsers();
+    }
+    increaseUsers() {
+        User._allUsers++;
+    }
+    get getUserName() {
+        return this.name;
+    }
+    get getAllUsersNumber() {
+        return User._allUsers;
+    }
+    set setAllUsersNumber(newNumber) {
+        if (newNumber === User._allUsers) {
+            User._allUsers = newNumber;
+        }
+        else {
+            throw new Error("Users number and the current number can't be the same ");
+        }
+    }
+}
+User._allUsers = 0; // shared props to all instances
+const logger = (msg) => {
+    console.log(msg);
+};
+// initialization
+const user1 = new User("Daniel", "daniel@g.google", "234sdfa");
+const user2 = new User("Kemisola", "Kemisola@g.google", "as2323");
+logger(user2.getAllUsersNumber);
--- tsfile_p/src/index.ts
+++ tsfile_p/src/index.ts
@@ -1,1 +1,42 @@
-console.log("Hello world");
+class User {
+  private static _allUsers = 0; // shared props to all instances
+  constructor(
+    public name: string,
+    public email: string,
+    public readonly id: string
+  ) {
+    this.name = name;
+    this.email = email;
+    this.id = id;
+    this.increaseUsers();
+  }
+
+  private increaseUsers(): void {
+    User._allUsers++;
+  }
+
+  get getUserName(): string {
+    return this.name;
+  }
+
+  get getAllUsersNumber(): number {
+    return User._allUsers;
+  }
+  set setAllUsersNumber(newNumber: number) {
+    if (newNumber === User._allUsers) {
+      User._allUsers = newNumber;
+    } else {
+      throw new Error("Users number and the current number can't be the same ");
+    }
+  }
+}
+
+const logger = (msg: string | number): void => {
+  console.log(msg);
+};
+
+// initialization
+const user1: User = new User("Daniel", "daniel@g.google", "234sdfa");
+const user2: User = new User("Kemisola", "Kemisola@g.google", "as2323");
+
+logger(user2.getAllUsersNumber);
