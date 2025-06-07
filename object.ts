3eadfad171238f2b0b057e4f17b03c3b2ed240ad
Daniel Olatunde <olatundedaniel943@gmail.com>
2025-06-07 05:29:39 +0100

    optional object property

--- object.ts
+++ object.ts
@@ -17,16 +17,38 @@
 // console.log(createCourse());
 
 //-----------------------------------------------
-// type alias
+// // type alias
+// type User = {
+//   name: string;
+//   email: string;
+//   isActive: boolean;
+// };
+
+// // take obj of type of User and return obj of type of User as well
+// function createUser(user: User): User {
+//   return { name: "", email: "", isActive: false };
+// }
+// createUser({ name: "", email: "", isActive: false });
+
+// ----------------------------------------------
+// readonly
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  creditCardDetails?: number; // ? optional.  Could be set or not
 };
 
-// take obj of type of User and return obj of type of User as well
-function createUser(user: User): User {
-  return { name: "", email: "", isActive: false };
-}
-createUser({ name: "", email: "", isActive: false });
+let newUser: User = {
+  _id: "12345",
+  name: "Daniel",
+  email: "daniel@gmail.com",
+  isActive: true,
+  // No complain as creditCardDetails not ser -- optional
+};
+// accessing and modifying
+newUser.name = "gloria";
+// newUser._id = newUser._id + "changed"; // error, can't change a readonly property
+
 export {};
